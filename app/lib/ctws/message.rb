70b0e28ec9ebe6d62f3b4be5024faeeeb64fee64
Agustí B.R <agusti.br@coditramuntana.com>
2017-09-27 16:56:59 +0200

    [added] Message account_doesnt_exist

--- app/lib/ctws/message.rb
+++ app/lib/ctws/message.rb
@@ -3,39 +3,43 @@ module Ctws
     def self.not_found(record = 'record')
       "Sorry, #{record} not found."
     end
-    
+
     def self.unmatched_route(route = 'route')
       "No route matches #{route}"
-    end    
-    
+    end
+
     def self.invalid_credentials
       'Invalid credentials'
     end
-    
+
     def self.invalid_token
       'Invalid token'
     end
-    
+
     def self.missing_token
       'Missing token'
     end
-    
+
     def self.unauthorized
       'Unauthorized request'
     end
-    
+
     def self.account_created
       'Account created successfully'
     end
-    
+
     def self.account_not_created
       'Account could not be created'
     end
-    
+
+    def self.account_doesnt_exist
+      "Account doesn't exist"
+    end
+
     def self.authenticated_user_success
       'Authenticated user successfully'
     end
-    
+
     def self.expired_token
       'Sorry, your token has expired. Please login to continue.'
     end
--- lib/ctws/version.rb
+++ lib/ctws/version.rb
@@ -1,3 +1,3 @@
 module Ctws
-  VERSION = '0.1.11.alpha'
+  VERSION = '0.1.12.alpha'
 end
