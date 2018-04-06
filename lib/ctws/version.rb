2730f7f0c18b62b32f630ad4d0cd6902a5d35ee1
oliver <oliver.hv@coditramuntana.com>
2018-04-06 13:04:43 +0200

    [DOC] Update version.

--- lib/ctws/version.rb
+++ lib/ctws/version.rb
@@ -1,3 +1,7 @@
 module Ctws
-  VERSION = '0.2.0.beta'
+  # Under development: BETA
+
+  VERSION = '0.2.1.beta'
+  # PATCH - 0.2.1.beta - 6/4/2018
+  # [FEATURE] Add exception handler for ArgumentError.
 end
