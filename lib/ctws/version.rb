29357f7435ba8c96e30bb856aa936a7d6fd4a538
Isaac Massot <isaac.mg@coditramuntana.com>
2018-05-14 15:38:05 +0200

    add fr locale

--- lib/ctws/version.rb
+++ lib/ctws/version.rb
@@ -4,6 +4,7 @@ module Ctws
   VERSION = '0.2.2.beta'
 
   # PATCH - 0.2.2.beta - 14/5/2018
+  # [ADDED] Add fr locale.
   # [REFACTOR] Return error_codes as integers instead of strings.
 
   # PATCH - 0.2.1.beta - 06/4/2018
